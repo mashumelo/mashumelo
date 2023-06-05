0298c30642d2badeb419a77a2ddbffdde2754aa7
mashumelo <waylondn@proton.me>
2023-06-05 03:07:37 -0400

    corrections to code within logging. discordbot.py

--- Portfolio - Python/Discord Bot/discordbot.py
+++ Portfolio - Python/Discord Bot/discordbot.py
@@ -24,7 +24,7 @@ limit = 10
 
 #GIPHY GIFS
 def get_gif(searchTerm):
-    response = requests.get("https://api.giphy.com/v1/gifs/search?api_key=%s&q=%s&limit=%s" % (GIPHY_API_KEY, searchTerm, limit))
+    response = requests.get(f"https://api.giphy.com/v1/gifs/search?api_key={GIPHY_API_KEY}&q={searchTerm}&limit={limit}")
     data = response.json()
 
 
@@ -38,17 +38,16 @@ def get_gif(searchTerm):
 @bot.event
 async def on_ready():
 
-    #Creates a counter to keep track of number of servers the bot is in 
-      guild_count = 0
+    #Determines the number of servers/guilds the bot is in
+    guild_count = len(bot.guilds)
 
-      #Loops through all the servers the bot is associated with
-      for guild in bot.guilds:
+    #Loops through all the servers the bot is associated with
+    for guild in bot.guilds:
         #Print the servers ID and name
         print(f'{guild.id} - (name: {guild.name})')
-        #Increments the counter for the number of servers the bot is in
-        guild_count = guild_count + 1
-        #Prints how many servers the bot is in
-        print(f'There are {guild_count} servers that Mashbot is in.')
+            
+    #Prints how many servers the bot is in
+    print(f'There are {guild_count} servers that Mashbot is in.')
 
 
 #Event listener for when a new message is sent to a channel
@@ -73,6 +72,58 @@ async def on_message(message):
 
         else:
             await message.channel.send("Sorry, I couldn't find a gif for that search term.")
+
+
+# Event listener for when a member's attributes are updated
+@bot.event
+async def on_member_update(before, after):
+    # Check if the roles list has changed
+    if before.roles != after.roles:
+        # Get the difference between the before and after roles lists
+        added_roles = [role for role in after.roles if role not in before.roles]
+        removed_roles = [role for role in before.roles if role not in after.roles]
+
+        # Log the role changes to a separate channel
+        if added_roles:
+            role_names = [role.name for role in added_roles]
+            role_colors = [role.color for role in added_roles]
+            for i in range(len(role_names)):
+                embed = discord.Embed(title=f"{after.name} was given the {role_names[i]} role", color=discord.Color.blue())
+                if role_colors[i] != discord.Color.default():
+                    embed.description = f" (color: {role_colors[i]})"
+                channel = bot.get_channel(1014448478425993250)
+                await channel.send(embed=embed)
+
+        if removed_roles:
+            role_names = [role.name for role in removed_roles]
+            role_colors = [role.color for role in removed_roles]
+            for i in range(len(role_names)):
+                embed = discord.Embed(title=f"{after.name} was removed from the {role_names[i]} role", color=discord.Color.blue())
+                if role_colors[i] != discord.Color.default():
+                    embed.description = f" (color: {role_colors[i]})"
+                channel = bot.get_channel(1014448478425993250)
+                await channel.send(embed=embed)
+
+    # Check if the role colors have changed
+    for role in after.roles:
+        if role in before.roles and role.color != before.roles[before.roles.index(role)].color:
+            embed = discord.Embed(title=f"The {role.name} role color was updated for {after.name} (new color: {role.color})")
+            channel = bot.get_channel(1014448478425993250)
+            await channel.send(embed=embed)
+
+# Event listener for when a member's voice state is updated
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if before.channel != after.channel:
+        if before.channel:
+            embed = discord.Embed(title="Voice Channel Leave", description=f"{member.name} left voice channel {before.channel.name}", color=discord.Color.red())
+            channel = bot.get_channel(1014448478425993250)
+            await channel.send(embed=embed)
+
+        if after.channel:
+            embed = discord.Embed(title="Voice Channel Join", description=f"{member.name} joined voice channel {after.channel.name}", color=discord.Color.green())
+            channel = bot.get_channel(1014448478425993250)
+            await channel.send(embed=embed)
 
 
 #--- Main ---
